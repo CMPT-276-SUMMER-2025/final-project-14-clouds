f1074347b16700328e6f6b73fb2084f34742b2d9
catmango <derickson3040@gmail.com>
2025-07-22 13:34:28 -0700

    added translink API

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/translink_api': {
+        target: 'https://gtfsapi.translink.ca',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/translink_api/, '')
+      }
+    }
+  }
 })
