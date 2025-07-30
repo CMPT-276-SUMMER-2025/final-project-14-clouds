ea759a6152af0469223415eb8e1eab2727e4475d
kdrsid <mpl6@sfu.ca>
2025-07-29 22:27:01 -0700

    updated server file for cancel event

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,27 +1,35 @@
 import notificationapi from 'notificationapi-node-server-sdk';
 
-
 export default async function handler(req, res) {
 
   const data = req.body;
   console.log('Received data:', data);
-
-  try {
-    const result = await notificationapi.send({
-      type: 'map_events_for_websites',
-      to: { id: data.id },
-      web_push: {
-        title: data.msg,
-        message: data.msg,
-        icon: 'https://app.notificationapi.com/mstile-150x150.png?id={{commentId}}',
-        url: 'http://localhost:5173/',//need to change link
-      },
-      schedule: data.schedule
-    });
-
-    res.status(200).json(result.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to send notification' });
+  if (data.trackingid) {
+    try {
+      const result = await notificationapi.deleteSchedule(data.trackingid);
+      res.status(200).json(result.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to cancel notification' });
+    }
+  } else {
+    try {
+      const result = await notificationapi.send({
+        type: 'map_events_for_websites',
+        to: { id: data.id },
+        web_push: {
+          title: data.msg,
+          message: data.msg,
+          icon: 'https://app.notificationapi.com/mstile-150x150.png?id={{commentId}}',
+          url: 'http://localhost:5173/',//need to change link
+        },
+        schedule: data.schedule
+      });
+      res.status(200).json(result.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to send notification' });
+    }
   }
+
 }
