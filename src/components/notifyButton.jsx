b53dcc4bd03293ec4ca93d2173c23001e2f308bd
kdrsid <mpl6@sfu.ca>
2025-07-29 22:14:03 -0700

    fixed cancel function in frontend & backend

--- src/components/notifyButton.jsx
+++ src/components/notifyButton.jsx
@@ -2,55 +2,68 @@ import { NotificationAPIProvider } from '@notificationapi/react'
 import { useState } from 'react'
 import { IoMdNotifications, IoMdNotificationsOff } from "react-icons/io";
 import "./notifyButton.css";
-function NotificationButton({busNumber, arrivalTime, arriveIn}) {
+function NotificationButton({busNumber, arrivalTime, arriveIn}) { //the notification btton component 
     const notificationapi = NotificationAPIProvider.useNotificationAPIContext();
     const [enabled, setEnabled] = useState(true);
     const [schedules, setSchdules] = useState([]);
 
-    const checkPerms = () => {
-    if (notificationapi) {
-        notificationapi.setWebPushOptIn(true);
-    }
-    const time = arrivalTime.toISOString();
-    setEnabled(prev => !prev);
-    if (enabled) {
-      new Notification(`${busNumber} will arrive soon`, {
-                body: `Arriving ${arriveIn}`,
-              });
-      sendNotification();
-    } else {
-      cancelNotification(schedules || '');
-    }
+    const checkPerms = () => {// function to ask for permission for notifications
+      if (notificationapi) {
+          notificationapi.setWebPushOptIn(true);
+      }
+
+      setEnabled(prev => !prev);
+      if (enabled) {
+        new Notification(`${busNumber} will arrive soon`, {
+                  body: `Arriving ${arriveIn}`,
+                }); //web push while on website
+        sendNotification(); //schedule a web push notification, it should work even if user is not on the website 
+      } else {
+        cancelNotification(schedules || '');
+      }
     };
     const cancelNotification = async (trackingId) => {
       if(trackingId) {
-        notificationapi.deleteSchedule(trackingId);
+        try {
+        const send = await fetch('http://localhost:3000/cancel-notification', { //change link to this when hosting '/api/send-notification'
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: 'kris68008@gmail.com',
+            trackingid: trackingId
+          })
+        });
         console.log('removed schedule notification', trackingId)
+      } catch (error) {
+        console.error('Error canceling notification:', error);
       }
     }
+  };
     const sendNotification = async () => {
-    const msg = `${busNumber} arriving now!`;
-    const s = arrivalTime.toISOString() || '';
-    console.log('schedule: ', s, busNumber);
-    try {
-      const res = await fetch('http://localhost:3000/send-notification', { //change link to this when hosting '/api/send-notification'
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          id: 'kris68008@gmail.com',
-          msg: msg || 'Bus arriving soon!' ,
-          schedule: s
-        })
-      });
+      const msg = `${busNumber} arriving now!`;
+      const s = arrivalTime.toISOString() || '';
+      console.log('schedule: ', s, busNumber);
+      try {
+        const res = await fetch('http://localhost:3000/send-notification', { //change link to this when hosting '/api/send-notification'
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: 'kris68008@gmail.com',
+            msg: msg || 'Bus arriving soon!' ,
+            schedule: s
+          })
+        });
 
-      const data = await res.json();
-      console.log('Notification sent:', data);
-      setSchdules(data.trackingId);
-    } catch (err) {
-      console.error('Error sending notification:', err);
-    }
+        const data = await res.json();
+        console.log('Notification sent:', data, data.trackingId);
+        setSchdules(data.trackingId);
+      } catch (error) {
+        console.error('Error sending notification:', error);
+      }
   };
 
   return (
