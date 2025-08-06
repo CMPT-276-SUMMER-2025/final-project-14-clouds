482c57964a56f457d1b2d3e07bff6d2b6bc92e60
kdrsid <mpl6@sfu.ca>
2025-08-06 14:59:46 -0700

    debug2

--- api/translinkapi.js
+++ api/translinkapi.js
@@ -2,7 +2,8 @@ export default async function handler(req, res) {
     const API_KEY = "DkOQ2I9r9TigGG9qoBLU";
     const GTFS_REALTIME_URL = `https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey=${API_KEY}`;
     const url = req.body + API_KEY;
-    console.log("url:", req.body)
+    console.log("data:", req.body);
+    console.log("url", url);
     try {
         const response = await fetch(url);
         const arrayBuffer = await response.arrayBuffer();
