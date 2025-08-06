9555865c2612d1635cf39edce52cb632c2bde069
kdrsid <mpl6@sfu.ca>
2025-08-06 14:36:24 -0700

    fixed passing wrong url to back end

--- api/translinkapi.js
+++ api/translinkapi.js
@@ -1,13 +1,7 @@
 export default async function handler(req, res) {
     const API_KEY = "DkOQ2I9r9TigGG9qoBLU";
-    const GTFS_REALTIME_URL = `https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey=${API_KEY}`;
-    const GTFS_ALERTS_URL = `https://gtfsapi.translink.ca/v3/gtfsalerts?apikey=${API_KEY}`;
-    const type = req.body; 
-    if (type === 'realtime') {
-        url = GTFS_REALTIME_URL;
-    } else {
-        url = GTFS_ALERTS_URL;
-    }
+    const url = req.body + API_KEY; 
+
     try {
         const response = await fetch(url);
         const arrayBuffer = await response.arrayBuffer();
--- src/translink/translinkAPI.jsx
+++ src/translink/translinkAPI.jsx
@@ -2,8 +2,8 @@ import * as gtfsRealtimeBindings from 'gtfs-realtime-bindings';
 
 // THIS IS A TEMP SOLUTION, WE WILL NOT HAVE A HARDCODED API KEY IN FINAL VER
 const API_KEY = "DkOQ2I9r9TigGG9qoBLU";
-const GTFS_REALTIME_URL = `https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey=${API_KEY}`;
-const GTFS_ALERTS_URL = `https://gtfsapi.translink.ca/v3/gtfsalerts?apikey=${API_KEY}`;
+const GTFS_REALTIME_URL = `https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey=`;
+const GTFS_ALERTS_URL = `https://gtfsapi.translink.ca/v3/gtfsalerts?apikey=`;
 
 export let alertCache = {}
 
