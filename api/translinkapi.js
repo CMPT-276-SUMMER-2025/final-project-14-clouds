fd43c3a697eb126ff7969d789ffc5109b27a4a5e
kdrsid <mpl6@sfu.ca>
2025-08-06 14:08:51 -0700

    testing

--- api/translinkapi.js
+++ api/translinkapi.js
@@ -4,7 +4,11 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(GTFS_REALTIME_URL);
-        res.status(200).json(response);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        res.setHeader('Content-Type', 'application/octet-stream');
+        res.status(200).send(buffer);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch data from translinkapi' });
