19b5cddc4cb5f9a33c182100654e47ee8af4376e
kdrsid <mpl6@sfu.ca>
2025-08-06 13:47:18 -0700

    wrong var for return data

--- api/translinkapi.js
+++ api/translinkapi.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(GTFS_REALTIME_URL);
-        res.status(200).json(Buffer.from(data));
+        res.status(200).json(response.data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch data from translinkapi' });
